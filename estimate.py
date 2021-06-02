6d1a49c7e92cffa301a583130842d979312452c3
SADHINSALEEM <64057801+SADHINSALEEM@users.noreply.github.com>
2021-06-02 14:54:22 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -30,3 +30,32 @@ class TestMC(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+
+
+
+def wallis(n):
+    o=int(n)
+    x=1
+    pi=1
+    for n in range(o):
+        w=0
+        w=4*x*x
+        pi*=w/(w-1)
+        x+=1
+    return 2*pi 
+
+
+
+def monte_carlo(n):
+    incircle=0
+    outcircle=0
+    o=int(n)
+    for n in range(o):
+        x=random.random()
+        y=random.random()
+        if (((x**2+y**2)**0.5)>1) :
+            outcircle+=1
+        else:
+            incircle+=1
+    pi=4*(incircle/(outcircle+incircle))
+    return pi
